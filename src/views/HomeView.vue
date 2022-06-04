f25108f9f0f3b7aebcda2e33a6f039e3a045c217
Biahellens <biancahellen03@icloud.com>
2022-06-04 14:38:12 -0300

    Atualização da page Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
-import TheWelcome from "@/components/TheWelcome.vue";
+import Cards from '@/components/Cards.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="search">
+      <h3>Relação de Operadoras Ativas ANS</h3>
+      <input class="searchANS" placeholder="pesquisar operadoras" />
+    </div>
+    <Cards />
   </main>
 </template>
+<style>
+  .search {
+    min-height: 5rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  .searchANS {
+    height: 2rem;
+    width: 20rem;
+    border-radius: 0.5rem;
+    text-align: center;
+  }
+
+  h3 {
+    color: #4682b4;
+  }
+</style>
